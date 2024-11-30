67c1c245d89a0dcd777831f8078e9400d2d24892
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2024-11-30 11:14:10 -0500

    testing board

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 console.log('the test works!');
 
-var sensor = require('node-dht-sensor');
+const { Board, Thermometer } = require('johnny-five');
+const board = new Board();
 
-sensor.read(11, 4, function (err, temperature, humidity) {
-  if (!err) {
-    console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-  } else {
-    console.log('error haz happenz');
-  }
+console.log(board);
+
+board.on('ready', () => {
+  console.log('board is ready');
+  const thermometer = new Thermometer({
+    controller: 'DHT11_I2C_NANO_BACKPACK',
+  });
+  console.log(thermometer);
+  console.log('yes we got here awaiting on data change');
+  thermometer.on('change', () => {
+    const { celsius, fahrenheit, kelvin } = thermometer;
+    console.log('Thermometer');
+    console.log('  celsius      : ', celsius);
+    console.log('  fahrenheit   : ', fahrenheit);
+    console.log('  kelvin       : ', kelvin);
+    console.log('--------------------------------------');
+  });
 });
