0cecb657d12c64d84afedea903abacf0f91f0694
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2024-11-30 13:08:11 -0500

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log('the test works!');
 
 var sensor = require('node-dht-sensor');
 
-sensor.read(11, 26, function (err, temperature, humidity) {
+sensor.read(11, 4, function (err, temperature, humidity) {
   console.log(temperature);
   console.log(humidity);
   if (!err) {
@@ -13,24 +13,24 @@ sensor.read(11, 26, function (err, temperature, humidity) {
   }
 });
 
-// const { Board, Thermometer } = require('johnny-five');
-// const board = new Board();
+const { Board, Thermometer } = require('johnny-five');
+const board = new Board();
 
-// console.log(board);
+console.log(board);
 
-// board.on('ready', () => {
-//   console.log('board is ready');
-//   const thermometer = new Thermometer({
-//     controller: 'DHT11_I2C_NANO_BACKPACK',
-//   });
-//   console.log(thermometer);
-//   console.log('yes we got here awaiting on data change');
-//   thermometer.on('change', () => {
-//     const { celsius, fahrenheit, kelvin } = thermometer;
-//     console.log('Thermometer');
-//     console.log('  celsius      : ', celsius);
-//     console.log('  fahrenheit   : ', fahrenheit);
-//     console.log('  kelvin       : ', kelvin);
-//     console.log('--------------------------------------');
-//   });
-// });
+board.on('ready', () => {
+  console.log('board is ready');
+  const thermometer = new Thermometer({
+    controller: 'DHT11_I2C_NANO_BACKPACK',
+  });
+  console.log(thermometer);
+  console.log('yes we got here awaiting on data change');
+  thermometer.on('change', () => {
+    const { celsius, fahrenheit, kelvin } = thermometer;
+    console.log('Thermometer');
+    console.log('  celsius      : ', celsius);
+    console.log('  fahrenheit   : ', fahrenheit);
+    console.log('  kelvin       : ', kelvin);
+    console.log('--------------------------------------');
+  });
+});
