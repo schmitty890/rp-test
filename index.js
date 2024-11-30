029672d0b7c94dd74d8aebc548a0b6c00e46632f
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2024-11-30 14:19:31 -0500

    test

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-console.log('the test works!');
+console.log('testing . . . ');
 
-// var sensor = require('node-dht-sensor');
+var sensor = require('node-dht-sensor');
 
-// sensor.read(11, 4, function (err, temperature, humidity) {
-//   console.log(temperature);
-//   console.log(humidity);
-//   if (!err) {
-//     console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-//   } else {
-//     console.log('an error has occured');
-//     console.log(err);
-//   }
-// });
-
-const sensor = require('node-dht-sensor').promises;
-sensor.setMaxRetries(10);
-sensor.initialize(11, 4);
-async function exec() {
-  try {
-    const res = await sensor.read(11, 4);
+sensor.read(11, 4, function (err, temperature, humidity) {
+  if (!err) {
     console.log(
-      `temp: ${res.temperature.toFixed(1)}°C, ` +
-        `humidity: ${res.humidity.toFixed(1)}%`
+      `temp: ${temperature.toFixed(1)}°C, ` +
+        `humidity: ${humidity.toFixed(1)}%`
     );
-  } catch (err) {
-    console.error('Failed to read sensor data:', err);
   }
-}
-
-exec();
+});
