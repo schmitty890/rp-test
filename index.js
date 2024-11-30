75fb36e420eec615f161dddadb01776a6bfa2304
Jason Schmitt <jasonschmitt@Jasons-MBP-2.lan>
2024-11-30 12:04:28 -0500

    testing

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var sensor = require('node-dht-sensor');
 sensor.read(11, 4, function (err, temperature, humidity) {
   if (!err) {
     console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+  } else {
+    console.log('an error has occured');
+    console.log(err);
   }
 });
 
